e6f3f2eb1f73456789127adfb5e3beac3a417c1d
Fernandes T G Rodrigues <fernandes@email.com>
2025-12-17 21:18:57 -0400

    Finished the LLM course module

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import argparse
 
 
 load_dotenv()
@@ -12,9 +14,29 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
+    parser = argparse.ArgumentParser(description="AI Agent Chatbot written in Python")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    #parser.add_argument("--verbose", type=bool, )
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+
+    #user_prompt = args.user_prompt
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(response.text)
+    model='gemini-2.5-flash', contents=messages)
+
+    if response is None:
+        raise RuntimeError("usage_metadata is None")
+    else:
+        usage = response.usage_metadata
+        if not args.verbose:
+            print(f"Response: {response.text}")
+        else:
+            print(f"User prompt: {messages}\nPrompt tokens: {usage.prompt_token_count}\nResponse tokens: {usage.candidates_token_count}\nResponse:\n{response.text}")
+        
 
 if __name__ == "__main__":
     main()
